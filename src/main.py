f73788b0767a2a2336481f374d0173815a2f66ab
Marco Bischoff <bischoff_m@aim.com>
2025-05-03 01:07:11 +0200

    Clean up

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.random.randn(10, 5))
-print("Test Data:")
-print(df)
+if __name__ == "__main__":
+    df = pd.DataFrame(np.random.randn(10, 5))
+    print("Test Data:")
+    print(df)
 
-# Save the data to a CSV file
-root_path = Path(__file__).parent.parent
-df.to_csv(root_path / "data.csv", index=False)
+    # Save the data to a CSV file
+    root_path = Path(__file__).parent.parent
+    df.to_csv(root_path / "data.csv", index=False)
